58c6cc8f576a5eec151056f67e21613b83d7c7a8
kavya <kavya.marripudi@gmail.com>
2018-08-03 09:21:05 +0000

    Test

--- v12_DeployedVersion/app.js
+++ v12_DeployedVersion/app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true});
+mongoose.connect("mongodb://kavya:samsungabc@123@ds153841.mlab.com:53841/yelpcamp", {useNewUrlParser: true});
+ 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
